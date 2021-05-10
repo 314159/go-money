e6346372d02bab6ff8187c03fd811d739ea074f6
Scott Nelson <sbnelson@thermeon.com>
2021-05-10 12:06:40 -0500

    Make error messages begin with lowercase letters
    per go lint

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,8 +8,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ## [Unreleased]
 ### Added
 ### Changed
+- Made error messages begin with lowercase letter per go lint.
 ### Deprecated
 ### Removed
 ### Fixed
 ### Security
+
 <!-- markdownlint-configure-file { "MD022": false, "MD024": false, "MD030": false, "MD032": false} -->
--- money/money.go
+++ money/money.go
@@ -54,7 +54,7 @@ func NewMoney(amount string, c Currency) (Money, error) {
 		integerPart = parts[0]
 		decimalPart = parts[1]
 	default:
-		return nil, fmt.Errorf("Invalid Money amount: %s", amount)
+		return nil, fmt.Errorf("invalid Money amount: %s", amount)
 	}
 
 	// TODO: strip off trailing zeros from decimalPart so "1.00" dp 0 is okay
@@ -69,12 +69,12 @@ func NewMoney(amount string, c Currency) (Money, error) {
 		break
 	default:
 		// This would cause loss of precision!
-		return nil, fmt.Errorf("Invalid Money amount: %s", amount)
+		return nil, fmt.Errorf("invalid Money amount: %s", amount)
 	}
 
 	v, err := strconv.ParseInt(integerPart+decimalPart, 10, 64)
 	if err != nil {
-		return nil, fmt.Errorf("Invalid Money amount: %s", amount)
+		return nil, fmt.Errorf("invalid Money amount: %s", amount)
 	}
 
 	m := money{value: v, currency: c}
